073915b48688ff27f7f7c2ef7dd91d01a11f474f
erenspc <erensapci2009@gmail.com>
2025-11-18 00:52:48 +0300

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1e40af',
+        secondary: '#1e3a8a',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
