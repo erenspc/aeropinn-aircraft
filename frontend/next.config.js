7cf1c0b7ba0dcdaa2ec2530eff6e074a7dc0cd0d
erenspc <erensapci2009@gmail.com>
2025-11-18 01:20:34 +0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,18 +5,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  webpack: (config, { isServer }) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      path: false,
-      os: false,
-    };
-    return config;
-  },
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
-  },
 };
 
 module.exports = nextConfig;
